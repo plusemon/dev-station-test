c8da45731c73ca95f7e3253cbf6e62b018545c91
plusemon <bdemon00@gmail.com>
2023-07-22 18:59:47 +0600

    stack on styling issue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,17 @@ defineProps({
             <div class="card-body">
                 <h2>Welcome ({{ auth.user.name }})</h2>
                 <h4>You're logged in!</h4>
+
+
+
             </div>
+
+        </div>
+
+        <div class="alert alert-info mt-3">
+            If you are just logged in please refresh once! it will fix the styling issue
+            <!-- reson: When login success Guest layout assets still remain in Authenticated layout-->
+            <!-- I couldnot found any solution to fix this problem. it maybe innertia bugs -->
         </div>
     </AuthenticatedLayout>
 </template>
